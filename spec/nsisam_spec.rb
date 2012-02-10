b4939a382971189b4d66b1d6dd74212fe99299cb
Douglas Camata <d.camata@gmail.com>
2012-02-10 17:37:22 -0200

    specs for handling invalid keys added

--- spec/nsisam_spec.rb
+++ spec/nsisam_spec.rb
@@ -11,36 +11,56 @@ describe NSISam do
     @keys.each { |key| @nsisam.delete(key) }
   end
 
-  it "can store a value in SAM" do
-    response = @nsisam.store("something")
-    response.should_not be_nil
-    response.should have_key("key")
-    response.should have_key("checksum")
+  context "storing" do
+    it "can store a value in SAM" do
+      response = @nsisam.store("something")
+      response.should_not be_nil
+      response.should have_key("key")
+      response.should have_key("checksum")
 
-    @keys.push(response["key"])
+      @keys.push(response["key"])
+    end
   end
 
-  it "can delete a stored value" do
-    key = @nsisam.store("delete this")["key"]
-    response = @nsisam.delete(key)
-    response["deleted"].should be_true
+  context "deleting" do
+    it "can delete a stored value" do
+      key = @nsisam.store("delete this")["key"]
+      response = @nsisam.delete(key)
+      response["deleted"].should be_true
+    end
+
+    it "raises error when key not found" do
+      expect { @nsisam.delete("i dont exist") }.to raise_error(NSISam::Errors::Client::KeyNotFoundError)
+    end
   end
 
-  it "can retrieve a stored value" do
-    key = @nsisam.store("retrieve this")["key"]
-    response = @nsisam.get(key)
-    response["data"].should == "retrieve this"
+  context "retrieving" do
+    it "can retrieve a stored value" do
+      key = @nsisam.store("retrieve this")["key"]
+      response = @nsisam.get(key)
+      response["data"].should == "retrieve this"
 
-    @keys.push(key)
+      @keys.push(key)
+    end
+
+    it "raises error when key not found" do
+      expect { @nsisam.get("non existing key") }.to raise_error(NSISam::Errors::Client::KeyNotFoundError)
+    end
   end
 
-  it "can update values in keys already stored" do
-    key = @nsisam.store("update this")["key"]
-    response = @nsisam.update(key, "updated")
-    response["key"].should == key
-    response.should have_key("checksum")
+  context "updating" do
+    it "can update values in keys already stored" do
+      key = @nsisam.store("update this")["key"]
+      response = @nsisam.update(key, "updated")
+      response["key"].should == key
+      response.should have_key("checksum")
 
-    @keys.push(key)
+      @keys.push(key)
+    end
+
+    it "raises error when key not found" do
+      expect { @nsisam.update("ruby is fast", "foo") }.to raise_error(NSISam::Errors::Client::KeyNotFoundError)
+    end
   end
 
 end
