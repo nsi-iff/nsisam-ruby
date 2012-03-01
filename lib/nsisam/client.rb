80b6f047e99a0e42a988ceaf50c61cddb36d3b20
Douglas Camata <d.camata@gmail.com>
2012-03-01 17:57:36 -0300

    client public methods 100% documented

--- lib/nsisam/client.rb
+++ lib/nsisam/client.rb
@@ -20,24 +20,45 @@ module NSISam
     #
     # @param [String] data the desired data to store
     # @return [Hash] response with the data key and checksum
+    #   * "key" [String] the key to access the stored data
+    #   * "checksum" [String] the sha1 checksum of the stored data 
     def store(data)
       request_data = {:value => data}.to_json
       request = prepare_request :PUT, request_data
       execute_request(request)
     end
 
+    # Delete data at a given SAM key
+    #
+    # @param [Sring] key of the value to delete
+    # @return [Hash] response
+    #   * "deleted" [Boolean] true if the key was sucefully deleted 
     def delete(key)
       request_data = {:key => key}.to_json
       request = prepare_request :DELETE, request_data
       execute_request(request)
     end
 
+    # Recover data stored at a given SAM key
+    #
+    # @param [String] key of the value to acess
+    # @return [Hash] response
+    #   * "from_user" [String] the user who stored the value
+    #   * "date" [String] the date when the value was stored
+    #   * "data" [String, Hash, Array] the data stored at that key
     def get(key)
       request_data = {:key => key}.to_json
       request = prepare_request :GET, request_data
       execute_request(request)
     end
 
+    # Update data stored at a given SAM key
+    #
+    # @param [String] key of the data to update
+    # @param [String, Hash, Array] data to be stored at the key
+    # return [Hash] response
+    #   * "key" [String] just to value key again
+    #   * "checksum" [String] the new sha1 checksum of the key's data
     def update(key, value)
       request_data = {:key => key, :value => value}.to_json
       request = prepare_request :POST, request_data
